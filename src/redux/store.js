40532ef6ac172f6fe5196086f8c50a12e94098f0
Aritri Chatterjee <73156770+aritrichatterjee9@users.noreply.github.com>
2025-01-29 19:11:38 +0530

    thunk fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'; // For Redux DevTools
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk'; // Use default import for thunk
 import { userReducer } from './reducer';
 
 // Create the Redux store with thunk and Redux DevTools
